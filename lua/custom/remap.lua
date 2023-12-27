f21766807fd4e5c3b1d8125b97b271123bd82335
bencouser <neb2010@live.co.uk>
2023-12-27 22:10:01 +0000

    starting to add settings and bindings

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -1,1 +1,13 @@
+-- To Files
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Inset Semi
+vim.keymap.set('n', '<leader>;', 'A;<Esc>')
+
+-- Search and Keep Center
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Copy to Clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -1,1 +1,15 @@
+-- Relative Line Numbers ON
 vim.wo.relativenumber = true
+
+-- Square Boi
+vim.opt.guicursor = ""
+
+-- No Wrapping
+vim.opt.wrap = false
+
+-- Indending
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true

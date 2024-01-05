ae9374acd8a44c17b58142a7b8260d96853e9a70
Ben <neb2010@live.co.uk>
2024-01-05 15:24:01 +0000

    wb

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -14,8 +14,8 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
--- Log Undo
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+-- -- Long Undo
+-- vim.opt.swapfile = false
+-- vim.opt.backup = false
+-- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- vim.opt.undofile = true

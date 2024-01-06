699e55a5504cc7865b75c091039557aa55cade94
bencouser <neb2010@live.co.uk>
2024-01-06 08:45:48 +0000

    cant get lsp working

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,39 +1,43 @@
 -- NOTE: This is where your plugins related to LSP can be installed.
 --  The configuration is done below. Search for lspconfig to find it below.
 return{
-        -- LSP Configuration & Plugins
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            -- Automatically install LSPs to stdpath for neovim
-            { 'williamboman/mason.nvim', config = true },
-            'williamboman/mason-lspconfig.nvim',
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        -- Automatically install LSPs to stdpath for neovim
+        { 'williamboman/mason.nvim', config = true },
+        'williamboman/mason-lspconfig.nvim',
 
-            -- Useful status updates for LSP
-            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-            { 'j-hui/fidget.nvim',       opts = {} },
+        -- Useful status updates for LSP
+        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+        { 'j-hui/fidget.nvim',       opts = {} },
 
-            -- Additional lua configuration, makes nvim stuff amazing!
-            'folke/neodev.nvim',
-        },
-        config = function()
-            -- [[ Configure LSP ]]
-            --  This function gets run when an LSP connects to a particular buffer.
-            local on_attach = function(_, bufnr)
-              local nmap = function(keys, func, desc)
+        -- Additional lua configuration, makes nvim stuff amazing!
+        'folke/neodev.nvim',
+    },
+    config = function()
+        -- [[ Configure LSP ]]
+        --  This function gets run when an LSP connects to a particular buffer.
+        local on_attach = function(_, bufnr)
+            local nmap = function(keys, func, desc)
                 if desc then
-                  desc = 'LSP: ' .. desc
+                    desc = 'LSP: ' .. desc
                 end
 
                 vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-              end
+            end
 
-              nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-              nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+            nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+            nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-              nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-              nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-              nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-              nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+            print("lsp config")
+
+            nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+            nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+            nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+
+            print("beflow config")
 
 
             require('mason').setup()
@@ -42,22 +46,22 @@ return{
             require 'lspconfig'.mojo.setup {}
 
             local servers = {
-              -- mojo = { "mojo" },mojo
-              -- clangd = {},
-              -- gopls = {},
-              -- pyright = {},
-              -- rust_analyzer = {},
-              -- tsserver = {},
-              -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+                -- mojo = { "mojo" },mojo
+                -- clangd = {},
+                -- gopls = {},
+                -- pyright = {},
+                -- rust_analyzer = {},
+                -- tsserver = {},
+                -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
-              lua_ls = {
-                Lua = {
-                  workspace = { checkThirdParty = false },
-                  telemetry = { enable = false },
-                  -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                  -- diagnostics = { disable = { 'missing-fields' } },
+                lua_ls = {
+                    Lua = {
+                        workspace = { checkThirdParty = false },
+                        telemetry = { enable = false },
+                        -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                        -- diagnostics = { disable = { 'missing-fields' } },
+                    },
                 },
-              },
             }
 
             -- Setup neovim lua configuration
@@ -70,19 +74,19 @@ return{
             local mason_lspconfig = require('mason-lspconfig')
 
             mason_lspconfig.setup {
-              ensure_installed = vim.tbl_keys(servers),
+                ensure_installed = vim.tbl_keys(servers),
             }
 
             mason_lspconfig.setup_handlers {
-              function(server_name)
-                require('lspconfig')[server_name].setup {
-                  capabilities = capabilities,
-                  on_attach = on_attach,
-                  settings = servers[server_name],
-                  filetypes = (servers[server_name] or {}).filetypes,
-                }
-              end,
+                function(server_name)
+                    require('lspconfig')[server_name].setup {
+                        capabilities = capabilities,
+                        on_attach = on_attach,
+                        settings = servers[server_name],
+                        filetypes = (servers[server_name] or {}).filetypes,
+                    }
+                end,
             }
-            end
+        end
     end
 }

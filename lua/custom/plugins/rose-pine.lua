b457f6d3ed0b8304805a596667afd4afa7267cc3
bencouser <neb2010@live.co.uk>
2024-02-06 18:05:30 +0000

    see through mode

--- lua/custom/plugins/rose-pine.lua
+++ lua/custom/plugins/rose-pine.lua
@@ -3,6 +3,9 @@ return {
 	name = 'rose-pine',
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme 'rose-pine'
+		vim.cmd.colorscheme('rose-pine')
+
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end,
 }
